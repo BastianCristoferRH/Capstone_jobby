6cf7bdd3c2b64a2758486725285f0a1aabeb8271
bastian <bastiancristofer98@gmail.com>
2023-09-28 00:15:25 -0300

    filtro

--- src/app/listar-servicios/listar-servicios.page.ts
+++ src/app/listar-servicios/listar-servicios.page.ts
@@ -1,20 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
-import { Router } from '@angular/router'; 
+import { Router } from '@angular/router';
 
-@Component({  
+@Component({
   selector: 'app-listar-servicios',
   templateUrl: './listar-servicios.page.html',
   styleUrls: ['./listar-servicios.page.scss'],
 })
 export class ListarServiciosPage implements OnInit {
-  listaServicios:any[] = [];
+  listaServicios: any[] = [];
+  terminoBusqueda: string = '';
   constructor(
-    private http: HttpClient ,
+    private http: HttpClient,
     private authService: AuthService,
     private router: Router
-    ) { }
+  ) { }
 
   ngOnInit() {
     this.cargarListadoServicios();
@@ -29,4 +30,47 @@ export class ListarServiciosPage implements OnInit {
     const perfilTrabajadorUrl = `/trabajador/${correoElectronico}`;
     this.router.navigate([perfilTrabajadorUrl]);
   }
+  argarListadoServicios() {
+    this.authService.cargarListadoServicios().subscribe((data: any) => {
+      this.listaServicios = data;
+      console.log(this.listaServicios);
+    });
+  }
+
+  // Función para filtrar la lista de servicios
+  filtrarServicios() {
+    const terminoBusquedaLowerCase = this.terminoBusqueda.toLowerCase().split(' ');
+  
+    // Filtra la lista de servicios en función de términos de búsqueda flexibles
+    this.listaServicios = this.listaServicios.filter((servicio) => {
+      const textoServicio = (
+        servicio.name_serv.toLowerCase() +
+        servicio.des_serv.toLowerCase() +
+        servicio.name_comuna.toLowerCase() +
+        servicio.name_region.toLowerCase()
+      );
+  
+      // Verifica si al menos un término está presente en el texto del servicio
+      return terminoBusquedaLowerCase.every(termino => textoServicio.includes(termino));
+    });
+  }
+  
+
+  // Función para limpiar el filtro y restablecer la lista original
+  limpiarFiltro() {
+    this.terminoBusqueda = ''; // Vacía el término de búsqueda
+    this.cargarListadoServicios(); // Recarga la lista de servicios original
+  }
+
+  buscarServicios() {
+   
+    if (this.terminoBusqueda.trim() !== '') {
+    
+      this.filtrarServicios();
+    }else if(this.terminoBusqueda.trim() == ''){
+      this.limpiarFiltro();
+    }
+    
+  }
 }
+
